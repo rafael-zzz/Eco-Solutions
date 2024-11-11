40d7b6f1436479517468c8d3c98f8ba029cb114d
Rafael Peixoto <rmp2@cesar.school>
2024-11-10 21:22:20 -0300

    Update settings.py

--- eco_storage/settings.py
+++ eco_storage/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+*^-3nxt3hl3wyu90#%zb(0pzya0k--nxt5$nc(k@(__wk652k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
